8f2c001c5bd5e27508e948e3f0238a0c78c03fe2
NotSoToxic <sarthak.joshi52@gmail.com>
2023-01-27 09:58:31 +0530

    .
    .

---
+++ SQL/Advanced Select/The PADS.sql
@@ -0,0 +1,7 @@
+select concat(name,'(',substring(Occupation,1,1),')') as Name 
+from occupations 
+order by Name;
+Select concat ('There are a total of ', count(occupation),' ', lower(occupation),'s.') as totals
+from occupations
+group by occupation
+order by totals
