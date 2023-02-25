d69116db833cd99b5d5d4c608a7b64e1169d9394
NotSoToxic <sarthak.joshi52@gmail.com>
2023-02-25 20:46:28 +0530

    Create Weather-Observation-Station-5.sql

---
+++ SQL/Basic Select/Weather-Observation-Station-5.sql
@@ -0,0 +1,4 @@
+# Author: Sarthak
+
+select city,length(city) from station order By length(city) asc, city asc limit 1;
+select distinct(City),length(city) from station order by length(city) desc, city asc limit 1;
