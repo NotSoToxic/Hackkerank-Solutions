eb051eebb6b2761533bced89325f06436cd37fc5
NotSoToxic <sarthak.joshi52@gmail.com>
2023-02-26 12:19:15 +0530

    Create Weather-Observation-Station-6.sql

---
+++ SQL/Basic Select/Weather-Observation-Station-6.sql
@@ -0,0 +1,11 @@
+# Author: Sarthak
+
+select distinct(city) from station where
+city like "a%" or
+city like "e%" or
+city like "i%" or
+city like "o%" or
+city like "u%";
+
+# this works faster and better
+select distinct city from station where left(city,1) in('a','e','i','o','u')
