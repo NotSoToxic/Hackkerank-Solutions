e65dde478c3077fbf291ec7ff1218c9431bd3f4c
NotSoToxic <sarthak.joshi52@gmail.com>
2023-06-22 00:42:10 +0530

    Update(FindingRunner-UpScore.py)

--- Python/FindingRunner-UpScore.py
+++ Python/FindingRunner-UpScore.py
@@ -1,7 +1,7 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    arr1 = list(arr)
+
+n = int(input())
+arr = map(int, input().split())
+arr1 = list(arr)
 n_arr = [
     number for number in arr1
     if  number < max(arr1)
